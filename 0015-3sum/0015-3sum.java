2d40a55f48f462c7d293f953e3e0ffcd80a7e993
junsun-yeam <wnstjs1031@naver.com>
2025-12-01 14:51:13 +0900

    Time: 31 ms (65.55%), Space: 59 MB (32.35%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int left, right, sum;
+        List<List<Integer>> results = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 중복된 값 건너뛰기
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            // 간격을 좁혀가며 합 sum 계산
+            left = i + 1;
+            right = nums.length - 1;
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0)
+                    left += 1;
+                else if (sum > 0)
+                    right -= 1;
+                else {
+                    results.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1])
+                        left += 1;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right -= 1;
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return results;
+    }
+}
