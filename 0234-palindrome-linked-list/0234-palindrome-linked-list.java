6d1ef37172c912139d8a6f0aa02a3d9c51423833
junsun-yeam <wnstjs1031@naver.com>
2025-12-03 12:19:30 +0900

    Time: 12 ms (15.35%), Space: 109.9 MB (5.97%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode node = head;
+        Deque<Integer> deque = new LinkedList<>();
+
+        while(node != null) {
+            deque.push(node.val);
+            node = node.next;
+        }
+
+        while(!deque.isEmpty() && deque.size() > 1) {
+            if(deque.pollFirst() != deque.pollLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
